087151bc0b0836f25c393eafd7a59e69ae75d359
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-18 00:51:44 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,14 +1,34 @@
 def alphabetize(arr)
   sort = []
-  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz "
   arr.each do |string|
+    j = 0
     if sort == []
-      sort.push(string)
-    elsif alphabet.index(string[0]) < alphabet.index(sort[0])
+      sort << string
+      next
+    end 
+    if alphabet.index(string[0]) < alphabet.index(sort[0][0])
       sort.unshift(string)
-    else 
+      next 
+    end 
+    while j <= sort.size - 1 
+      i = 0 
+      while i <= string.size - 1
+        if alphabet.index(string[i]) > alphabet.index(sort[j][i])
+          break
+        end
+        if alphabet.index(string[i]) < alphabet.index(sort[j][i])
+          sort.insert(j, string)
+          j = sort.size + 1
+          break
+        end
+        i += 1 
+      end
+      j += 1 
+    end 
+    if j == sort.size
       sort.push(string)
     end 
-  end 
+  end
   sort
 end
